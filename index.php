6102ea708efb800bf34a0789b2206e4dc5c17525
Heather Monatt <hmonatt@mail.greenriver.edu>
2021-02-20 20:30:22 -0500

    Added summary view

--- index.php
+++ index.php
@@ -21,5 +21,38 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
+//survey route
+$f3->route('GET /survey', function(){
+    //echo "Survey 1 Page";
+    $view = new Template();
+    echo $view->render('views/survey.html');
+});
+
+//summary route
+$f3->route('POST /summary', function() {
+
+    //echo "<p>POST:<p>";
+    //var_dump($_POST);           //post array only contains the most updated data
+
+    //echo "<p>SESSION:<p>";
+    //var_dump($_SESSION);        //session array most secure array for data
+
+    if(isset($_POST['name'])){
+        $_SESSION['name'] = $_POST['name'];
+    }
+
+
+    //add data from form 2 to session array
+    if(isset($_POST['survey'])){
+        $_SESSION['survey'] = implode(", ", $_POST['survey']);
+    }
+
+    //echo "Summary Route";
+    $view = new Template();
+    echo $view->render('views/survey-summary.html');
+
+});
+
+
 //Run fat free
 $f3->run();
